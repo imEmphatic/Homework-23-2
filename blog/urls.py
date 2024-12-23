abb0da392dc7427c0cc0de8ce65bc5aa0f478682
Aleksei Prusikov <imemphatic@yandex.ru>
2024-12-23 16:14:34 +0300

    edited urls.py from blog

--- blog/urls.py
+++ blog/urls.py
@@ -16,9 +16,9 @@ urlpatterns = [
     # Маршрут для создания нового поста (важно, чтобы он был выше динамического маршрута)
     path("post/new/", BlogPostCreateView.as_view(), name="post_create"),
     # Маршрут для просмотра деталей поста
-    path("post/<slug:slug>/", BlogPostDetailView.as_view(), name="post_detail"),
+    path("post/<int:pk>/detail/", BlogPostDetailView.as_view(), name="post_detail"),
     # Маршрут для редактирования поста
-    path("post/<slug:slug>/edit/", BlogPostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/edit/", BlogPostUpdateView.as_view(), name="post_update"),
     # Маршрут для удаления поста
-    path("post/<slug:slug>/delete/", BlogPostDeleteView.as_view(), name="post_delete"),
+    path("post/<int:pk>/delete/", BlogPostDeleteView.as_view(), name="post_delete"),
 ]
